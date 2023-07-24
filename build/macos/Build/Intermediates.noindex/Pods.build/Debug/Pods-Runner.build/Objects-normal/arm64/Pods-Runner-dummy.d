5a164469ee35e8ffba7327d7768b6a0ef4fb4e5c
Spchdt <spchdt@gmail.com>
2023-07-24 20:18:19 +0700

    delete files

--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/arm64/Pods-Runner-dummy.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/artties/Documents/CubeSat-Station/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap \
-  /Users/artties/Documents/CubeSat-Station/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m
--- build/macos/Build/Intermediates.noindex/Pods.build/Debug/simple_animation_progress_bar.build/Objects-normal/arm64/simple_animation_progress_bar-dummy.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/artties/Documents/CubeSat-Station/build/macos/Build/Intermediates.noindex/Pods.build/Debug/simple_animation_progress_bar.build/module.modulemap \
-  /Users/artties/Documents/CubeSat-Station/macos/Pods/Target\ Support\ Files/simple_animation_progress_bar/simple_animation_progress_bar-dummy.m \
-  /Users/artties/Documents/CubeSat-Station/macos/Pods/Target\ Support\ Files/simple_animation_progress_bar/simple_animation_progress_bar-prefix.pch
